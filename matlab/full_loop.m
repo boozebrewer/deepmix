31942e058c06a3e7f4c27604bd05e28924b6423c
Ben Golan <ben_golan@saips.co.il>
2016-10-25 22:46:19 +0300

    different layers experiments and report updated

--- matlab/full_loop.m
+++ matlab/full_loop.m
@@ -2,14 +2,18 @@
 % build spectrogram
 % send to aws for training
 %% init 
-HOSTNAME = '54.88.11.92';
+HOSTNAME = '54.243.26.94';
 % paths
-style_wav_path = 'audio/argov_6sec.wav';
-content_wav_path = 'audio/gov_6sec.wav';
+style_wav_path = 'scale_fm_6sec.wav';
+content_wav_path = 'bach_6sec.wav';
 style_path = 'png/style.png';
 content_path = 'png/content.png';
 algo_out_path = 'png/algo_output.png';
 algo_out_folder = 'png';
+%params
+weight=500; imsize=513; iters=250; init='image'; original_color =0;
+con_layers = 'relu4_2';%  Default is relu4_2
+sty_layers = 'relu1_1,relu2_1,relu3_1,relu4_1,relu5_1'; % Default relu1_1,relu2_1,relu3_1,relu4_1,relu5_1
 %% make content
 [xOrig, fsOrig] = audioread(content_wav_path);
 %playback x original
@@ -43,8 +47,7 @@ fprintf('saving size out png %dx%d\n', size(styleTosave,1),size(styleTosave,2));
 send_to_aws(style_path, content_path,HOSTNAME);
 %%
 % init='image' or 'random'
-weight=3000; imsize=513; iters=250; init='image'; original_color =0;
-run_aws(weight, imsize, iters, init,original_color,HOSTNAME);
+run_aws(weight, imsize, iters, init,original_color, con_layers, sty_layers, HOSTNAME);
 %%
 get_aws_image(algo_out_folder,HOSTNAME);
 %% show all spectras
@@ -52,12 +55,12 @@ show_spectrum_of_the_three(algo_out_path, content_path, style_path, t, f, style_
 [a,b1,c]=fileparts(style_wav_path);
 [a,b2,c]=fileparts(content_wav_path);
 ttl = ['style: ', b1, ' vs ', 'content: ', b2];
-paramstxt = sprintf('weight %d, imsize %d, iters %d, init %s, original_color %d',weight, imsize, iters, init,original_color);
+paramstxt = sprintf('weight %d, imsize %d, iters %d, init %s, o_c %d, s_lay %s, c_lay %s',weight, imsize, iters, init,original_color, sty_layers, con_layers);
 runname = [ttl, '  params-> ', paramstxt];
 mysuptitle(runname);
 valid_runname = mymakeValidName(runname);
 figname = sprintf('results/output/%s.fig',valid_runname);
-savefig(figname);
+% savefig(figname);
 saveas(gcf,[figname,'.jpg']);
 %% load
 bL = loadspect(algo_out_path,contentTosave,t,f);
@@ -65,7 +68,7 @@ bL = loadspect(algo_out_path,contentTosave,t,f);
 xReco = reconstruct(bL,mx,mi,ymax,yabs,yphase,h,nfft,fs,y,x,nrm_factor);
 title(runname,'interpreter','none');
 figname = sprintf('results/output/%s_time.fig',valid_runname);
-savefig(figname);
+% savefig(figname);
 saveas(gcf,[figname,'.jpg']);
 audiowrite([figname,'.wav'],xReco,fs);
 %% play
